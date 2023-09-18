6efc85c7fe1608d2aa60986e44e298fe9bb48e42
glebokhlopkov <glebka_90@mail.ru>
2023-09-18 12:07:06 +0300

    create class_to_file.py

--- class_api.py
+++ class_api.py
@@ -1,6 +1,6 @@
 import requests
-import json
 from abc import ABC, abstractmethod
+from class_to_file import SaveToJSONFile
 import os
 
 
@@ -11,22 +11,24 @@ class BaseAPI(ABC):
         pass
 
 
-class HeadHunterAPI(BaseAPI):
+class HeadHunterAPI(BaseAPI, SaveToJSONFile):
 
     def __init__(self):
         self.url_address = 'https://api.hh.ru/vacancies'
+        SaveToJSONFile.__init__(self)
 
     def get_vacancies(self):
         vacancies_dict = requests.get(self.url_address)  # headers={'HH-User-Agent': 'MyApp/1.0 (my-app-feedback@example.com)'})
-        return vacancies_dict.json()
+        return vacancies_dict.text
 
 
-class SuperJobAPI(BaseAPI):
+class SuperJobAPI(BaseAPI, SaveToJSONFile):
     SJ_API_KEY = os.getenv('SuperJob_API_KEY')
 
     def __init__(self):
         self.url_address = 'https://api.superjob.ru/2.0/vacancies/'
+        SaveToJSONFile.__init__(self)
 
     def get_vacancies(self):
         vacancies_dict = requests.get(self.url_address, headers={'X-Api-App-Id': self.SJ_API_KEY}, params={'count': 100})
-        return vacancies_dict.json()
+        return vacancies_dict.text
