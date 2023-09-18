9addecb1a5f8de278702cc3a0bf86b76e415822e
glebokhlopkov <glebka_90@mail.ru>
2023-09-18 17:36:10 +0300

    create main.py with correct code in another files

--- class_vacancies.py
+++ class_vacancies.py
@@ -1,11 +1,20 @@
-class Vacancy():
+from class_to_file import SaveToJSONFile
+import json
+
+
+class Vacancy(SaveToJSONFile):
     def __init__(self, vac_dict):
         self.name = vac_dict['profession']
         self.url = vac_dict['client']['link']
         self.salary = vac_dict['payment_from'], vac_dict['payment_to']
         self.info = vac_dict['candidat']
         self.published_time = vac_dict['date_published']
-        self.town = vac_dict['town']['tittle']
+        self.town = vac_dict['town']['title']
+        SaveToJSONFile.__init__(self)
+
+    def save_to_file(self, data_dict):
+        with open('choice_vacancies.json', 'a', encoding='utf-8') as json_file:
+            return json.dump(data_dict, json_file, indent=4)
 
     @classmethod
     def create_vacancy_from_dict(cls, vac_dict):
