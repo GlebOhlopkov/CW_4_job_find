8961b7c0cc95de9201ff2fe1d8377242e7a7b899
glebokhlopkov <glebka_90@mail.ru>
2023-09-19 13:15:53 +0300

    write docstrings and comments

--- class_api.py
+++ class_api.py
@@ -6,6 +6,9 @@ import os
 
 
 class BaseAPI(ABC):
+    """
+    Абстрактный класс для работы с API
+    """
 
     @abstractmethod
     def get_vacancies(self):
@@ -13,25 +16,48 @@ class BaseAPI(ABC):
 
 
 class HeadHunterAPI(BaseAPI, SaveToJSONFile):
+    """
+    Класс для работы с API сайта www.headhunter.ru
+    """
 
     def __init__(self):
+        """
+        Создание экземпляра класса сайта https://api.hh.ru/vacancies
+        """
         self.url_address = 'https://api.hh.ru/vacancies'
         SaveToJSONFile.__init__(self)
 
-    def get_vacancies(self):
+    def get_vacancies(self) -> dict:
+        """
+        Функция для получения данных сайта
+
+        :return данные в формате словаря
+        """
         response = requests.get(self.url_address)  # headers={'HH-User-Agent': 'MyApp/1.0 (my-app-feedback@example.com)'})
         vacancies_dict = json.loads(response.text)
         return vacancies_dict
 
 
 class SuperJobAPI(BaseAPI, SaveToJSONFile):
+    """
+    Класс для работы с API сайта www.superjob.ru
+    """
+
     SJ_API_KEY = os.getenv('SuperJob_API_KEY')
 
     def __init__(self):
+        """
+        Создание экземпляра класса сайта https://api.superjob.ru/2.0/vacancies/
+        """
         self.url_address = 'https://api.superjob.ru/2.0/vacancies/'
         SaveToJSONFile.__init__(self)
 
-    def get_vacancies(self):
+    def get_vacancies(self) -> dict:
+        """
+        Функция для получения данных сайта
+
+        :return данные в формате словаря
+        """
         response = requests.get(self.url_address,
                                 headers={'X-Api-App-Id': self.SJ_API_KEY},
                                 params={'count': 100, 'town': 'Москва', 'keywords': 'Python'})
--- class_to_file.py
+++ class_to_file.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 
 
 class SaveToFile(ABC):
+    """
+    Абстрактный класс для сохранения данных в файл
+    """
 
     @abstractmethod
     def save_to_file(self, data_dict):
@@ -10,7 +13,16 @@ class SaveToFile(ABC):
 
 
 class SaveToJSONFile(SaveToFile):
+    """
+    Класс для сохранения данных в json-файл
+    """
 
-    def save_to_file(self, data_dict):
+    def save_to_file(self, data_dict: dict) -> None:
+        """
+        Функция сохраняет выбранный словарь в json-файл
+
+        :param data_dict: словарь с данными в формате json
+        :return: запись данных в файл 'all_vacancies.json'
+        """
         with open('all_vacancies.json', 'w', encoding='utf-8') as json_file:
             return json.dump(data_dict, json_file, indent=4)
