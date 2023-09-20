61d1e052dbec226d463bc0036691700ac1371782
glebokhlopkov <glebka_90@mail.ru>
2023-09-20 20:03:45 +0300

    rework class_vacancies.py add classmethod for hh.ru and sj.ru correct __init__ add __str__ add __le__, __ge__

--- class_vacancies.py
+++ class_vacancies.py
@@ -1,36 +1,70 @@
-from class_to_file import SaveToJSONFile
-import json
-
-
-class Vacancy(SaveToJSONFile):
+class Vacancy:
     """
     Класс для создания вакансии
     """
 
-    def __init__(self, vac_dict: dict):
+    def __init__(self, vacancy_dict: dict):
         """
         Создание экземпляра класса вакансии
-        :param vac_dict: словарь с данными по профессии
+        :param vacancy_dict: словарь с данными по профессии
         """
-        self.name = vac_dict['profession']
-        self.url = vac_dict['client']['link']
-        self.salary = vac_dict['payment_from'], vac_dict['payment_to']
-        self.info = vac_dict['candidat']
-        self.published_time = vac_dict['date_published']
-        self.town = vac_dict['town']['title']
-        SaveToJSONFile.__init__(self)
+        self.name = vacancy_dict['name']
+        self.url = vacancy_dict['url']
+        self.salary_from = vacancy_dict['salary_from']
+        self.salary_to = vacancy_dict['salary_to']
+        self.info = vacancy_dict['info']
+        self.published_time = vacancy_dict['published_time']
+        self.town = vacancy_dict['town']
 
-    def save_to_file(self, data_dict: dict) -> None:
-        """
-        Функция сохраняет выбранный словарь в json-файл
+    def _str__(self):
+        return f'''Вакансия - {self.name},
+Сводная информация - {self.info}
+Зарплата - {self.salary_from} - {self.salary_to}
+Город - {self.town}
+Дата публикации - {self.published_time}
+Ссылка - {self.url}
+'''
 
-        :param data_dict: словарь с данными в формате json
-        :return: запись данных в файл 'choice_vacancies.json'
+    def __le__(self, other: int):
         """
-        with open('choice_vacancies.json', 'a', encoding='utf-8') as json_file:
-            return json.dump(data_dict, json_file, indent=4)
+        Метод для сравнения зарплаты вакансии
+        :param other: требуемое значение зарплаты
+        :return: True, если наивысшее из значений зарплаты вакансии меньше требуемой
+        """
+        return max(self.salary_from, self.salary_to) <= other
+
+    def __ge__(self, other: int):
+        """
+        Метод для сравнения зарплаты вакансии
+        :param other: требуемое значение зарплаты
+        :return: True, если наивысшее из значений зарплаты вакансии больше требуемой
+        """
+        return max(self.salary_from, self.salary_to) >= other
 
     @classmethod
-    def create_vacancy_from_dict(cls, vac_dict):
-        for vac in vac_dict:
-            cls(vac)
+    def create_vacancy_from_hh(cls, vac_dic_hh):
+        """Метод для создания экземпляра класса Vacancy из словаря сайта headhunter.ru"""
+        vacancy_info = {
+            'name': vac_dic_hh['name'],
+            'url': vac_dic_hh['alternate_url'],
+            'salary_from': vac_dic_hh['salary']['from'],
+            'salary_to': vac_dic_hh['salary']['to'],
+            'info': vac_dic_hh['snippet']['requirement'],
+            'published_time': vac_dic_hh['published_at'],
+            'town': vac_dic_hh['area']['name'],
+        }
+        return Vacancy(vacancy_info)
+
+    @classmethod
+    def create_vacancy_from_sj(cls, vac_dic_sj):
+        """Метод для создания экземпляра класса Vacancy из словаря сайта superjob.ru"""
+        vacancy_info = {
+            'name': vac_dic_sj['profession'],
+            'url': vac_dic_sj['link'],
+            'salary_from': vac_dic_sj['payment_from'],
+            'salary_to': vac_dic_sj['payment_to'],
+            'info': vac_dic_sj['candidat'],
+            'published_time': vac_dic_sj['date_published'],
+            'town': vac_dic_sj['town']['title'],
+        }
+        return Vacancy(vacancy_info)
