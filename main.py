56e96d78442c6211f2b5a233f1d3dd4de64f6d09
glebokhlopkov <glebka_90@mail.ru>
2023-09-20 21:55:40 +0300

    rework main.py - create new json-files - correct work with API - correct information out add new func in class_to_file.py for load file add method in class_vacancies.py for check salary

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ from class_vacancies import Vacancy
 from class_to_file import SaveToJSONFile
 import json
 
-# создание экземпляра для работы с API сайта superjob.ru
-sj_api = SuperJobAPI()
-# файл для сохранения вакансий с сайта superjob.ru
-sj_json_path = 'sj_all_vacancies.json'
-# создание экземпляра для сохранения вакансий sj.ru в файл
-sj_save_all_vac = SaveToJSONFile(sj_json_path)
-# получение вакансий связанных с Python
-sj_vacancies = sj_api.get_vacancies()
-# сохранение всех вакансий sj.ru в файл
-sj_save_all_vac.save_to_file(sj_vacancies)
 
 # создание экземпляра для работы с API сайта headhunter.ru
 hh_api = HeadHunterAPI()
@@ -25,6 +15,26 @@ hh_vacancies = hh_api.get_vacancies()
 # сохранение всех вакансий hh.ru в файл
 hh_save_all_vac.save_to_file(hh_vacancies)
 
+# создание экземпляра для работы с API сайта superjob.ru
+sj_api = SuperJobAPI()
+# файл для сохранения вакансий с сайта superjob.ru
+sj_json_path = 'sj_all_vacancies.json'
+# создание экземпляра для сохранения вакансий sj.ru в файл
+sj_save_all_vac = SaveToJSONFile(sj_json_path)
+# получение вакансий связанных с Python
+sj_vacancies = sj_api.get_vacancies()
+# сохранение всех вакансий sj.ru в файл
+sj_save_all_vac.save_to_file(sj_vacancies)
+
+# файл для сохранения отфильтрованных вакансий с сайта headhunter.ru
+hh_choice = 'hh_choice_vacancy.json'
+# файл для сохранения отфильтрованных вакансий с сайта superjob.ru
+sj_choice = 'sj_choice_vacancy.json'
+# создание экземпляра для сохранения отфильтрованных вакансий hh.ru в файл
+hh_save_choice_vac = SaveToJSONFile(hh_choice)
+# создание экземпляра для сохранения отфильтрованных вакансий sj.ru в файл
+sj_save_choice_vac = SaveToJSONFile(sj_choice)
+
 
 def user_interaction():
     """
@@ -35,33 +45,50 @@ def user_interaction():
     # Запрос у пользователя необходимых критериев
     need_salary = int(input("Введите требуемую зарплату: "))
 
-    # Обработка полученных вакансий с требованиями пользователя
-    with open('all_vacancies.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        # Новый словарь для обработанных вакансий
-        new_vac_dict = {'vacancy': []}
-        for vac in data:
-            new_vac = Vacancy(vac)
+    # Обработка полученных вакансий сайта headhunter.ru с требованиями пользователя
+    data_hh = hh_save_all_vac.load_file()
+    # Новый словарь для обработанных вакансий
+    new_vac_dict_hh = {'vacancy': []}
+    for vac in data_hh:
+        if type(vac['salary']) == dict:
+            new_vac = Vacancy.create_vacancy_from_hh(vac)
             # Проверка требований пользователя
-            if min(new_vac.salary) >= need_salary:
-                new_vac_dict['vacancy'].append(vac)
+            if new_vac.check_salary(need_salary):
+                new_vac_dict_hh['vacancy'].append(vac)
             else:
                 continue
-        # Запись отфильтрованных вакансий в отдельный файл
-        with open('choice_vacancies.json', 'a', encoding='utf-8') as new_file:
-            json.dump(new_vac_dict, new_file, indent=4)
+    # Запись отфильтрованных вакансий в отдельный файл
+    hh_save_choice_vac.save_to_file(new_vac_dict_hh)
+
+    # Обработка полученных вакансий сайта superjob.ru с требованиями пользователя
+    data_sj = sj_save_all_vac.load_file()
+    # Новый словарь для обработанных вакансий
+    new_vac_dict_sj = {'vacancy': []}
+    for vac in data_sj:
+        new_vac = Vacancy.create_vacancy_from_sj(vac)
+        # Проверка требований пользователя
+        if new_vac.check_salary(need_salary):
+            new_vac_dict_sj['vacancy'].append(vac)
+        else:
+            continue
+    # Запись отфильтрованных вакансий в отдельный файл
+    sj_save_choice_vac.save_to_file(new_vac_dict_sj)
 
     print('Возможны следующие предложения:')
+    print('--- HeadHunter.ru ---')
+    # Вывод обработанных вакансий сайта headhunter.ru
+    choice_hh = hh_save_choice_vac.load_file()
+    for count, vac in enumerate(choice_hh['vacancy']):
+        new_vac = Vacancy.create_vacancy_from_hh(vac)
+        print(f'---> Вакансия №{count+1}\n{new_vac}')
+    print('<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n')
 
-    # Вывод обработанных вакансий
-    with open('choice_vacancies.json', 'r', encoding='utf-8') as new_file:
-        data = json.load(new_file)
-        for vac in data['vacancy']:
-            new_vac = Vacancy(vac)
-            print(new_vac.name)
-            print(new_vac.salary)
-            print(new_vac.info)
-            print('___')
+    print('--- SuperJob.ru: ---')
+    # Вывод обработанных вакансий сайта superjob.ru
+    choice_sj = sj_save_choice_vac.load_file()
+    for count,vac in enumerate(choice_sj['vacancy']):
+        new_vac = Vacancy.create_vacancy_from_sj(vac)
+        print(f'---> Вакансия №{count+1}\n{new_vac}')
 
 
 if __name__ == "__main__":
